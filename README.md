2d3c27a4f867f078f4ef0084e816318a992106fb
Brandon Castagna <bcastagna@springeq.com>
2020-10-29 16:05:32 -0400

    formatting + data sample

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring EQ Code Challenge
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const data = require("../data/sample.json");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+	res.render("index", {
+		title: "Express"
+	});
 });
+
+router.get("/data", function(req, res, next) {
+	res.json(data);
+})
 
 module.exports = router;
